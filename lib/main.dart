1d6dff78749c0fb5b485411608f6bbd92588267a
Rahul Shah <iq.nit.student@gmail.com>
2023-01-05 14:51:29 +0530

    second update

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,219 @@
-import 'package:benam/homeScreen.dart';
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:io';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Form Generator',
+      home: FormGenerator(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class FormGenerator extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _FormGeneratorState createState() => _FormGeneratorState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+class _FormGeneratorState extends State<FormGenerator> {
+  final _formKey = GlobalKey<FormState>();
+  Map<String, dynamic>? _formData;
+  void generateForm(String jsonInput) {
+    Map<String, dynamic> parsedJson = jsonDecode(jsonInput);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _formData = parsedJson;
+      _isLoading = false;
+    });
+  }
+
+  Future<void> dataStr() async {
+    final String response = await rootBundle.loadString('assets/test.json');
+    generateForm(response);
+    print(response);
+  }
+
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await dataStr();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-   
-    return HomeScreen();
+    return _isLoading
+        ? const Center(child: CircularProgressIndicator())
+        : _buildHomePage();
+  }
+
+  Widget _buildHomePage() {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_formData!['title']),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            // Display form description
+            // Generate form fields based on JSON input
+            Column(
+              children: [
+                Text(_formData!['desc']),
+                for (var field in _formData!['fields'])
+                  _buildFormField(context, field),
+              ],
+            ),
+            // Add submit and cancel buttons
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  ElevatedButton(
+                    child: Text(_formData!['cancelButtonText']),
+                    onPressed: () {
+                      // Navigate to previous screen
+                    },
+                  ),
+                  ElevatedButton(
+                    child: Text(_formData!['submitButtonText']),
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        // Send form data to API
+                        Uri url = Uri.parse(
+                          _formData!['submitButtonAPI'],
+                        );
+                        http.Response response = await http.post(url);
+                        if (response.statusCode == 200) {
+                          print('Success');
+                        } else {
+                          print('Failed');
+                        }
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFormField(BuildContext context, Map<String, dynamic> field) {
+    switch (field['type']) {
+      case 'textbox':
+        return Container(
+          height: 60,
+          width: MediaQuery.of(context).size.width - 30,
+          margin: EdgeInsets.fromLTRB(7.5, 7.5, 7.5, 7.5),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black),
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: TextFormField(
+            validator: (value) {
+              for (var validation in field['validations']) {
+                if (validation['length'] != null) {
+                  if (value!.length < validation['length']['greaterThan']) {
+                    return 'This field must be at least ${validation['length']['greaterThan']} characters long';
+                  }
+                }
+              }
+              return null;
+            },
+            decoration: InputDecoration(labelText: field['label']),
+          ),
+        );
+        break;
+      case 'slider':
+        return Container(
+          child: Slider(
+            activeColor: Colors.purple,
+            inactiveColor: Colors.purple.withOpacity(0.2),
+            value: _formData![field['label']] ??
+                double.parse(field['range']['min'].toString()),
+            min: double.parse(field['range']['min'].toString()),
+            max: double.parse(field['range']['max'].toString()),
+            onChanged: (value) {
+              setState(() {
+                _formData![field['label']] = value;
+              });
+            },
+            // validator: (value) {
+            //   for (var validation in field['validations']) {
+            //     if (validation['max'] != null && value > validation['max']) {
+            //       return 'This field must be less than or equal to ${validation['max']}';
+            //     }
+            //     if (validation['min'] != null && value < validation['min']) {
+            //       return 'This field must be greater than or equal to ${validation['min']}';
+            //     }
+            //     if (validation['required'] != null && validation['required']) {
+            //       if (value == null) {
+            //         return 'This field is required';
+            //       }
+            //     }
+            //   }
+            //   return null;
+            // },
+          ),
+        );
+        break;
+      case 'dropdown':
+        return Container(
+          height: 60,
+          width: MediaQuery.of(context).size.width - 30,
+          margin: EdgeInsets.fromLTRB(7.5, 7.5, 7.5, 7.5),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black),
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: DropdownButtonFormField(
+            value: _formData![field['label']] ?? field['values'][0]['value'],
+            items: field['values']
+                .map<DropdownMenuItem<String>>(
+                    (value) => DropdownMenuItem<String>(
+                          value: value['value'],
+                          child: Text(value['label']),
+                        ))
+                .toList(),
+            onChanged: (value) {
+              setState(() {
+                _formData![field['label']] = value;
+              });
+            },
+            validator: (value) {
+              for (var validation in field['validations']) {
+                if (validation['required'] != null && validation['required']) {
+                  if (value == null) {
+                    return 'This field is required';
+                  }
+                }
+              }
+              return null;
+            },
+          ),
+        );
+        break;
+      default:
+        return Text('Invalid field type');
+    }
   }
 }
